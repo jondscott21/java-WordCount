57eeec72f1ad255ef96c66a583efce68dac4a6da
Jonathan Scott <jonathan.scott@gmail.com>
2019-09-03 14:31:14 -0700

    mvp and stretch finished

--- src/wordcount/WordCount.java
+++ src/wordcount/WordCount.java
@@ -1,6 +1,29 @@
 package wordcount;
 
-class WordCount 
+public class WordCount 
 {
-    
+   private String declaration;
+
+   public WordCount(String declaration)
+   {
+       this.declaration = declaration;
+   }
+
+   public String getDeclaration() {
+       return declaration;
+   }
+
+   public void setDeclaration(String declaration) {
+       this.declaration = declaration;
+   }
+
+   @Override
+    public String toString()
+    {
+        return "Words{" +
+            "declaration='" + declaration + '\'' +
+            // ", avgWeight=" + avgWeight +
+            // ", apartment=" + apartment +
+            '}';
+    }
 }
